1e563aa4cdec051c8b1ab370571868362b9b72f2
Anna Flores <annaflowersk@gmail.com>
2023-08-29 20:18:19 -0500

    Create Checkpoint1.swift
    Initial checkpoint for 100 Days of Swift challenge

---
+++ Checkpoint1.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  100 Days Of Swift
+//
+//  Created by Anna Flores on 8/29/23.
+//
+
+import Foundation
+
+
+let temperatureInCelsius: Double = 39.0
+let fahrenheitConversion: Double = ((temperatureInCelsius * 9) / 5) + 32
+print("Celsius: \(temperatureInCelsius)° \nFahrenheit: \(fahrenheitConversion)°")
